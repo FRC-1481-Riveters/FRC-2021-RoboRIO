71490e2494483136e74ea6a46c0b784f3737033d
dakott@pobox.com <dakott@pobox.com>
2021-03-22 20:53:56 -0400

    reset x and y value idk hi

--- src/main/java/frc/robot/commands/AutonRamsetePath.java
+++ src/main/java/frc/robot/commands/AutonRamsetePath.java
@@ -14,9 +14,9 @@ import edu.wpi.first.wpilibj.DriverStation;
 
 public class AutonRamsetePath extends RamseteCommand {
 
-  private static Trajectory trajectory;
-  private static DriveTrain driveTrain;
-  private static boolean stopAfter;
+  private Trajectory trajectory;
+  private DriveTrain driveTrain;
+  private boolean stopAfter;
 
   public AutonRamsetePath( DriveTrain m_drive, Trajectory m_trajectory, boolean m_stopAfter )
     {
@@ -42,9 +42,9 @@ public class AutonRamsetePath extends RamseteCommand {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
+    super.initialize();
     driveTrain.resetOdometry( trajectory.getInitialPose() );
     DriverStation.reportError("Robot X set to " + driveTrain.m_odometry.getPoseMeters().getX() +  "  Robot Y set to " +  driveTrain.m_odometry.getPoseMeters().getY(), false);
-    super.initialize();
   }
 
   // Called every time the scheduler runs while the command is scheduled.
